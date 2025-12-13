6c1ae947d17c389d9a7ccef5b1087b1f7a57c537
Ehab Yasser <ehabyasser679@gmail.com>
2025-12-13 11:14:36 +0200

    Add files via upload

---
+++ MySQLServer.py
@@ -0,0 +1,58 @@
+import mysql.connector
+from mysql.connector import errorcode
+import getpass
+
+HOST = "localhost"
+USER = "root"
+
+DATABASE = "alx_book_store"
+
+def create_database():
+    PASSWORD = getpass.getpass ("Enter password: ")
+    conncetion = None
+    cursor = None
+    
+    try:
+        print (f"\nConnecting to MySQL server at {HOST} as user {USER}")
+        connection = mysql.connector.connect(
+            host = HOST,
+            user = USER,
+            password = PASSWORD
+        )
+
+        cursor = connection.cursor()
+
+        sql_query = f"Create database if not exists {DATABASE}"
+
+        cursor.execute(sql_query)
+
+        print (f"\nDatabase '{DATABASE}' created successfully")
+
+    except mysql.connector.Error as error:
+        if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print (f"\nError {error.errno}: {error.msg}")
+            print (f"\nYou don't have the necessary permissions to create a database: {error}")
+        
+        elif error.errno == errorcode.CR_CONNECTION_ERROR:
+            print (f"\nError {error.errno}:{error.msg}")
+            print (f"\nFailed to connect to MySQL server: {error}")
+       
+        elif error.errno == errorcode.ER_BAD_DB_ERROR:
+            print (f"\nError {error.errno}: {error.msg}")
+            print (f"\nDatabase does not exist: {error}")
+        
+        else:
+            print (f"\n Error {error.errno}: {error.msg}")
+            print (f"\n an unexpected error occurred during execution: {error}")
+    except Exception as error:
+        print (f"\n a genral error occurred during execution: {error}")
+
+    finally:
+        if cursor:
+            cursor.close()
+        if connection and connection.is_connected():
+            connection.close()
+            print ("\nConnection to MySQL server closed successfully")
+if __name__ == "__main__":
+    create_database()
+    
---
+++ alx_book_store.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT REFERENCES Authors(author_id),
+    price DOUBLE PRECISION,
+    publication_date DATE
+);
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT REFERENCES Customers(customer_id),
+    order_date DATE
+);
+
+CREATE TABLE Order_Details (
+    order_detail_id INT PRIMARY KEY,
+    order_id INT REFERENCES Orders(order_id),
+    book_id INT REFERENCES Books(book_id),
+    quantity DOUBLE PRECISION
+);
